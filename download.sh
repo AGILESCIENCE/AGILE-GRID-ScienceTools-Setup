13e5cdd95af2cd7c2dd50e88d2ddef98d34f82cd
Andrea Bulgarelli <bulgarelli@iasfbo.inaf.it>
2014-02-13 09:08:04 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,9 +1,8 @@
-
 git clone git@github.com:AGILESCIENCE/libagilepil.git 
 
 git clone git@github.com:AGILESCIENCE/libagilewcs.git    
 
-git clone git@github.com:AGILESCIENCE/libagilesci.git    
+git clone git@github.com:ASTRO-EDU/libagilesci.git    
 
 git clone git@github.com:AGILESCIENCE/agilesci1.git 
 
