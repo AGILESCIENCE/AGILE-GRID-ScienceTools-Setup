2fe05898f7584b340ce4b14c93009d0525514a8b
Andrea Bulgarelli <bulgarelli@iasfbo.inaf.it>
2015-06-25 15:55:32 +0200

    BUILD22

--- download.sh
+++ download.sh
@@ -1,9 +1,22 @@
+export TAG=BUILD22
+
 git clone git@github.com:AGILESCIENCE/libagilepil.git 
+cd libagilepil
+git checkout $TAG
+cd ..
 
 git clone git@github.com:AGILESCIENCE/libagilewcs.git    
+cd libagilewcs
+git checkout $TAG
+cd ..
 
 git clone git@github.com:ASTRO-EDU/libagilesci.git    
+cd libagilesci
+git checkout $TAG
+cd ..
 
 git clone git@github.com:AGILESCIENCE/agilesci1.git 
-
+cd agilesci1
+git checkout $TAG
+cd ..
 
