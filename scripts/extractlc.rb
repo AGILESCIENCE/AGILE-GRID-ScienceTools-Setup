28ba79561d87592ac5854e0aaf168a072df78768
Andrea Bulgarelli <bulgarelli@iasfbo.inaf.it>
2015-11-22 06:49:09 +0100

    bugs

--- scripts/extractlc.rb
+++ scripts/extractlc.rb
@@ -3,8 +3,6 @@
 #1) output file name
 #2) source name
 #3) min sqrt(TS) (optional, default 0)
-#4) l starting source (optional, default -999)
-#5) b starting source (optional, default -999)
 #quelli di seguito sono parametri opzionali, quindi da impostare tipo parametro=valore
 #6) radius: max distance from starting source (using l_peak, b_peak) (optional, default -999)
 #7) maxR: remove source if outside contour level (optional, default -999 none) - only if calculated - if >= 0 enable this option and use this number as systematic error
@@ -33,8 +31,7 @@ a.sort!
 ndim = a.size();
 
 minsqrtTS = ARGV[3]
-l = ARGV[4]
-b = ARGV[5]
+
 
 radius = -999;
 maxR = -999;
@@ -43,7 +40,7 @@ period = 0;
 minexp = -999;
 
 clean=0
-for i in 6...ARGV.size
+for i in 4...ARGV.size
 	if ARGV[i] == nil
 		break;
 	else
@@ -70,17 +67,12 @@ out1file = output.to_s + ".lc"
 outlcfile = File.new(out1file, "w");
 out6file = output.to_s + ".resfinalfull"
 outrfffile = File.new(out6file, "w");
-
-
-out0file = output.to_s + ".res"
-out = File.new(out0file, "w");
-
-out2file = output.to_s + ".short"
-out3 = File.new(out2file, "w");
 out3file = output.to_s + ".ob"
 outobfile = File.new(out3file, "w");
 out5file = output.to_s + ".tlist"
 outtlistfile = File.new(out5file, "w");
+
+
 
 
 startmjd = -1;
@@ -147,7 +139,7 @@ a.each do | xx |
 	end
 	
 	
-	if not (mo.sqrtTS.to_f >= minsqrtTS.to_f && ul.to_f != 0.0)
+	if not (mo.sqrtTS.to_f >= minsqrtTS.to_f && mo.flux_ul.to_f != 0.0)
 		next
 	end 
 	
@@ -183,59 +175,10 @@ a.each do | xx |
 	outlcob = format("%.6f", mo.timestart_tt.to_f) + "\t" + format("%.6f", mo.timestop_tt.to_f) + "\t" + obname.to_s + "\t" + mo.galcoeff.to_s + "\t" + mo.isocoeff.to_s + "\t" + lobs.to_s + "\t" + bobs.to_s + "\t" + mo.exposure.to_s + "\n";
 	outobfile.write(outlcob);
 	
-	#DA QUI IN AVANTI SISTEMARE PRENDENDO TUTTO DA mo
-	#la prima cosa da fare è eliminare if tstart.to_s != ""
-	#ora è mo.tstart, ma ce l'ho sempre 
-		
-	ul = 0.0;
-	exp = 0.0;
-	indexul = 0;
-	dimf = 0
-	
-	parama = ""
-	spectindex = 0
-	scindex = 0
-	scerror = 0
-	
-	ul = mo.flux_ul
-	gascoeff = mo.galcoeff
-	isocoeff = mo.isocoeff
-	spectindex = mo.sicalc.to_s + "+/-" + mo.sicalc_error.to_s
-	exp = mo.exposure
-	
-	
-				
-				
-	
-	outstr = format("%3.2f", mo.l_peak) + "\t" + format("%3.2f", mo.b_peak) + "\t" + format("%3.2f", mo.counts.to_f) + "\t" + format("%3.2f", mo.counts_error.to_f) + "\t" + format("%3.2f", mo.sqrtTS.to_f) + "\t" +  format("%.2e", mo.flux.to_f) + "\t" +  format("%.2e", mo.flux_error.to_f) + "\t" +  format("%.2e", ul.to_f) + "\t" + format("%.2f", gascoeff.to_f) + "\t" + format("%.2f", isocoeff.to_f) + "\t" + format("%3.2f", d3.to_f) + "\t" + obname.to_s + "\t" + format("%.4f", phase.to_f) + "\t" + format("%.2f", timestartmjd) + "-" + format("%.2f", timestopmjd) + "\t" + format("%.2f",distfov);
-		
-	outstrb = tstart.to_s + "-" + tstop.to_s  + "\t"  + format("%.6f", timestarttt.to_f) + "\t" + format("%.6f", timestoptt.to_f) + "\t" + lobs.to_s + "\t" + bobs.to_s + "\t" + ellipseres.chomp.to_s + "\t" + parama.chomp.to_s + "\t" + spectindex.to_s + "\t" + exp.to_s + "\n"
-		
-	if mo.sqrtTS.to_f >= minsqrtTS.to_f && ul.to_f != 0.0
-		puts "OUT " + outstr.to_s + "\t" + outstrb.to_s
-		out.write(outstr.chomp + "\t");
-		out.write(outstrb.chomp + "\n");
-		
-		#out.write("\n")
-	end
-	outstr3 = format("%3.2f", mo.l_peak) + "\t" + format("%3.2f", mo.b_peak) + "\t" + "\t" + format("%3.2f", mo.sqrtTS.to_f) + "\t" +  format("%.2e", mo.flux.to_f) + "\t" +  format("%.2e", mo.flux_error.to_f) + "\t" +  format("%.2e", ul.to_f) + "\t"  + format("%3.2f", d3.to_f) + "\t" + obname.to_s + "\t" + format("%.4f", phase) + "\t" + format("%.2f", timestartmjd) + "-" + format("%.2f", timestopmjd) + "\t" + tstart + "-" + tstop  + "\t" + format("%.2f",distfov) + "\t" + spectindex.to_s + "\t" + exp.to_s + "\n"
-	#timestarttt.to_s + "\t"
-	if mo.sqrtTS.to_f >= minsqrtTS.to_f && ul.to_f != 0.0
-		out3.write(outstr3);
-# 						puts outstr3
-	end
-	
-
-	
-			
-	
-	
-	
-	
 	index = index + 1;
 	
 end
-out.close();
+outobfile.close();
 outlcfile.close();
 outtlistfile.close();
 outrfffile.close();
