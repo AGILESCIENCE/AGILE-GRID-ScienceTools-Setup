0a2beedbccf23d43afb63593e5e7696de158f7e7
Andrea Bulgarelli <bulgarelli@iasfbo.inaf.it>
2015-12-18 11:22:46 +0100

    file size

--- scripts/indexloggen.rb
+++ scripts/indexloggen.rb
@@ -21,7 +21,9 @@ fout = File.new(dir + "INDEX/EVT.index", "w")
 
 Dir[dir + "/EVT/*.gz"].each do | file |
         puts file
-        fits.readFitsHeader(file);
-        fout.write(file + " " + fits.tt_start + " " + fits.tt_end + " EVT\n");
+	if File.size(file).to_i != 0
+        	fits.readFitsHeader(file);
+        	fout.write(file + " " + fits.tt_start + " " + fits.tt_end + " EVT\n");
+	end
 end
 fout.close()
