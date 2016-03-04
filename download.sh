8a0e1d8310dd287cb4a1d9a3a692c3e9335d83ad
Andrea Zoli <zoli@iasfbo.inaf.it>
2016-03-04 18:29:52 +0100

    Release BUILD23.

--- download.sh
+++ download.sh
@@ -2,25 +2,28 @@
 
 trap exit ERR
 
-TAG=master
-#TAG=BUILD23
+## BUILD23 ##
+TAG_PIL=v1.0.1
+TAG_WCS=v1.0.1
+TAG_LIBSCI=v1.2.0
+TAG_SCI1=v1.1.0
 
 git clone git@github.com:AGILESCIENCE/libagilepil.git 
 cd libagilepil
-git checkout $TAG
+git checkout $TAG_PIL
 cd ..
 
 git clone git@github.com:AGILESCIENCE/libagilewcs.git    
 cd libagilewcs
-git checkout $TAG
+git checkout $TAG_WCS
 cd ..
 
 git clone git@github.com:ASTRO-EDU/libagilesci.git    
 cd libagilesci
-git checkout $TAG
+git checkout $TAG_LIBSCI
 cd ..
 
 git clone git@github.com:AGILESCIENCE/agilesci1.git 
 cd agilesci1
-git checkout $TAG
+git checkout $TAG_SCI1
 cd ..
--- profile
+++ profile
@@ -1,3 +1,3 @@
-export AGILE=~/local.agile
+export AGILE=~/local_agile
 export PFILES=$AGILE/share
-export PATH=$AGILE/bin:$AGILE/scripts:$AGILE/scripts/sor:$PATH
+export PATH=$AGILE/bin:$AGILE/scripts:$AGILE/scripts/sor:$AGILE/scripts/extendesources:$AGILE/scripts/polygonfilter:$PATH
