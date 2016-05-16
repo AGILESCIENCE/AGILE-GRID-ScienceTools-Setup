3f45ae0a2503d930ed21c01dcb9761655cd9dddf
Andrea Zoli <zoli@iasfbo.inaf.it>
2016-05-16 18:35:33 +0200

    Set buildindex.sh correct path.

--- system/buildindex.sh
+++ system/buildindex.sh
@@ -7,11 +7,11 @@
 # and is strictly private and confidential. All rights reserved.
 
 log_index=/ASDC_PROC2/DATA_2/INDEX/LOG.log.index
-$AGILE/AG_indexgen /ASDC_PROC2/DATA_2/LOG LOG ${log_index}_tmp
+$AGILE/bin/AG_indexgen /ASDC_PROC2/DATA_2/LOG LOG ${log_index}_tmp
 sort -k3 ${log_index}_tmp -o ${log_index}_tmp
 mv ${log_index}_tmp ${log_index}
 
 evt_index=/ASDC_PROC2/FM3.119_2/INDEX/EVT.index
-$AGILE/AG_indexgen /ASDC_PROC2/FM3.119_2/EVT EVT ${evt_index}_tmp
+$AGILE/bin/AG_indexgen /ASDC_PROC2/FM3.119_2/EVT EVT ${evt_index}_tmp
 sort -k3 ${evt_index}_tmp -o ${evt_index}_tmp
 mv ${evt_index}_tmp ${evt_index}
