ebc10e136ceb165554300dd00fa2edc41d198dca
Andrea Bulgarelli <bulgarelli@iasfbo.inaf.it>
2016-08-22 17:04:10 +0200

    new format for .lc

--- scripts/MultiOutput.rb
+++ scripts/MultiOutput.rb
@@ -740,7 +740,7 @@ class MultiOutputList
 			mjdsize = multioutput.timestop_mjd.to_f - multioutput.timestart_mjd.to_f
 			mjdcenter = multioutput.timestart_mjd.to_f + mjdsize.to_f / 2.0
 			#calculate distance between lpointing, bpointing to l, b. NOW is -1
-			flc.write(flux.to_s + " " + fluxerror.to_s  + " " + fluxtype.to_s  + " " + mjdcenter.to_s  + " " + mjdsize.to_s + " " + distpointcalc.to_s + " " + runname + " " + multioutput.sqrtTS + " " + multioutput.exposure.to_s + " " + multioutput.galcoeff.chomp.to_s + " " + multioutput.isocoeff.chomp.to_s + " " + multioutput.dist.to_s + " ( " + multioutput.fullellipseline  + " )\n");
+			flc.write(format("%.2E",flux.to_s) + " " + format("%.2E", fluxerror.to_s)  + " " + fluxtype.to_s  + " " + format("%.7f", mjdcenter.to_s)  + " " + mjdsize.to_s + " " + distpointcalc.to_s + " " + runname + " " +  format("%.2f", multioutput.sqrtTS.to_s) + " " + format("%.2E", multioutput.exposure.to_s) + " " + multioutput.galcoeff.chomp.to_s + " " + multioutput.isocoeff.chomp.to_s + " " + multioutput.dist.to_s + " ( " + multioutput.fullellipseline  + " )\n");
 		end
 		f.close();
 		f1.close();
