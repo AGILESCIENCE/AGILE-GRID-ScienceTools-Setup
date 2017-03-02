635c64f9150aca198f7a396cbc5a8a0de967257a
Andrea Bulgarelli <bulgarelli@iasfbo.inaf.it>
2017-03-02 11:36:02 +0100

    BUILD24

--- download.sh
+++ download.sh
@@ -8,22 +8,22 @@ TAG_WCS=v1.0.2
 TAG_LIBSCI=v1.4.0
 TAG_SCI1=v1.4.0
 
-git clone git@github.com:AGILESCIENCE/libagilepil.git 
+git clone https://github.com/AGILESCIENCE/libagilepil.git 
 cd libagilepil
 git checkout $TAG_PIL
 cd ..
 
-git clone git@github.com:AGILESCIENCE/libagilewcs.git    
+git clone https://github.com/AGILESCIENCE/libagilewcs.git
 cd libagilewcs
 git checkout $TAG_WCS
 cd ..
 
-git clone git@github.com:ASTRO-EDU/libagilesci.git    
+git clone https://github.com/ASTRO-EDU/libagilesci.git    
 cd libagilesci
 git checkout $TAG_LIBSCI
 cd ..
 
-git clone git@github.com:AGILESCIENCE/agilesci1.git 
+git clone https://github.com/AGILESCIENCE/agilesc1.git
 cd agilesci1
 git checkout $TAG_SCI1
 cd ..
