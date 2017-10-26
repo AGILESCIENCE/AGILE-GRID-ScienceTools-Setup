980481f1afc801517bf8eede1b73ffc2a1c4679a
Nicolò Parmiggiani <nicolo.parmiggiani@gmail.com>
2017-10-26 22:11:01 +0200

    changes to lm6.rb

--- scripts/lm6.rb
+++ scripts/lm6.rb
@@ -113,24 +113,24 @@ edpmatrix = datautils.edpmatrix;
 	#S
 	tmin = tstart.to_f - parameters.t1s.to_f
 	tmax = tstart.to_f + parameters.t2s.to_f
-	cmd = "map.rb FM3.119_ASDC2_I0025 " + output + "_S " + tmin.to_s + " " + tmax.to_s + " "+l.to_s+" "+b.to_s+" emin="+parameters.emin.to_s+" emax="+parameters.emax.to_s+" filtercode="+parameters.filtercode.to_s+" timestep=1 mapsize=25 binsize=0.5 fovradmax="+parameters.fovradmax.to_s+" albedorad="+parameters.albedorad.to_s
+	cmd = "map.rb FM3.119_ASDCSTDkG_I0025 " + output + "_S " + tmin.to_s + " " + tmax.to_s + " "+l.to_s+" "+b.to_s+" emin="+parameters.emin.to_s+" emax="+parameters.emax.to_s+" filtercode="+parameters.filtercode.to_s+" timestep=1 mapsize=25 binsize=0.5 fovradmax="+parameters.fovradmax.to_s+" albedorad="+parameters.albedorad.to_s
 	datautils.execute("", cmd);
 
 	#B1
 	tmin = tstart.to_f - parameters.t1s.to_f - parameters.shiftt1b.to_f - parameters.t1b.to_f
 	tmax = tstart.to_f - parameters.t1s.to_f - parameters.shiftt1b.to_f
-	cmd = "map.rb FM3.119_ASDC2_I0025 " + output + "_B1 " + tmin.to_s + " " + tmax.to_s + " "+l.to_s+" "+b.to_s+" emin="+parameters.emin.to_s+" emax="+parameters.emax.to_s+" filtercode="+parameters.filtercode.to_s+" timestep=1 mapsize=25 binsize=0.5 fovradmax="+parameters.fovradmax.to_s+" albedorad="+parameters.albedorad.to_s
+	cmd = "map.rb FM3.119_ASDCSTDkG_I0025 " + output + "_B1 " + tmin.to_s + " " + tmax.to_s + " "+l.to_s+" "+b.to_s+" emin="+parameters.emin.to_s+" emax="+parameters.emax.to_s+" filtercode="+parameters.filtercode.to_s+" timestep=1 mapsize=25 binsize=0.5 fovradmax="+parameters.fovradmax.to_s+" albedorad="+parameters.albedorad.to_s
 	datautils.execute("", cmd);
 
 	#B2
 	tmin = tstart.to_f + parameters.t2s.to_f + parameters.shiftt2b.to_f
 	tmax = tstart.to_f + parameters.t2s.to_f + parameters.shiftt2b.to_f + parameters.t2b.to_f
-	cmd = "map.rb FM3.119_ASDC2_I0025 " + output + "_B2 " + tmin.to_s + " " + tmax.to_s + " "+l.to_s+" "+b.to_s+" emin="+parameters.emin.to_s+" emax="+parameters.emax.to_s+" filtercode="+parameters.filtercode.to_s+" timestep=1 mapsize=25 binsize=0.5 fovradmax="+parameters.fovradmax.to_s+" albedorad="+parameters.albedorad.to_s
+	cmd = "map.rb FM3.119_ASDCSTDkG_I0025 " + output + "_B2 " + tmin.to_s + " " + tmax.to_s + " "+l.to_s+" "+b.to_s+" emin="+parameters.emin.to_s+" emax="+parameters.emax.to_s+" filtercode="+parameters.filtercode.to_s+" timestep=1 mapsize=25 binsize=0.5 fovradmax="+parameters.fovradmax.to_s+" albedorad="+parameters.albedorad.to_s
 	datautils.execute("", cmd);
 
 
 	cmd = "cp " + PATH + "share/AG_lm6.par . "
 	datautils.execute("", cmd);
-	cmd = "export PFILES=.:$PFILES; "+PATH+"bin/AG_lm6 "+output+".lm "+output+"_S.cts.gz "+output+"_S.exp.gz "+output+"_B1.cts.gz "+output+"_B1.exp.gz "+output+"_B2.cts.gz "+output+"_B2.exp.gz "+l.to_s+" "+b.to_s+" 10 true 0 15 10"
+	cmd = "export PFILES=.:$PFILES; "+PATH+"bin/AG_lm6 "+output+".lm "+output+"_S.cts.gz "+output+"_S.exp.gz "+output+"_B1.cts.gz "+output+"_B1.exp.gz "+output+"_B2.cts.gz "+output+"_B2.exp.gz false "+l.to_s+" "+b.to_s+" 10 true false false 0 15 10"
 	puts cmd
 	datautils.execute("", cmd);
